50cb32263c1fdcd125ed611c5e4ab35bde30a59f
Kuvaldzin Kirill <kirkuv@mail.ru>
2024-06-26 15:15:46 +0300

    Phone do it

--- src/Home_Work_1/Phone.java
+++ src/Home_Work_1/Phone.java
@@ -3,15 +3,25 @@ package Home_Work_1;
 public class Phone {
     public static void main(String[] args) {
         int [] ph = {1,2,3,4,5,6,7,8,9,0};
-        printph(ph);
-        Integer[] ar = new Integer[ph.length];
-        for (int i = 0; i < ph.length; i++){
-            ar[i] = Integer.valueOf(ph[i]);
-            System.out.println(ph);
+        System.out.println(createPhoneNumber(ph));
+
         }
-    }
-    public static void printph (int[] ph)
-    {
-    //System.out.println(ph);
-    }
+
+    public static String createPhoneNumber (int[] ph)
+   {
+       String str="(";
+       for (int i= 0; i<3; i++)
+           str+=Integer.toString(ph[i]); // берем по одному символу
+       str+=") ";
+      // String str1="";
+       for (int i= 3; i<6; i++)
+           str+=Integer.toString(ph[i]);
+       //String str2 = "";
+       str+="-";
+       for (int i= 6; i<10; i++)
+           str+=Integer.toString(ph[i]);
+
+      // System.out.println("("+ str+ ")"+ " " +str1+ "-"+ str2);
+       return str;
+   }
 }
